41df70cd9e0edd17830b0e45f78096eea0bb0aab
Danielle Smith <smith.ddms@gmail.com>
2021-06-12 16:45:51 -0500

    Saturday Practice

--- practice/Program.cs
+++ practice/Program.cs
@@ -339,16 +339,76 @@ namespace practice
                     break;
                 Console.WriteLine(x);
             }*/
-                
+
+            /*Console.WriteLine("Try to guess my favorite number.");
+            Console.WriteLine("Enter your first guess to begin.");
+
+            string guess = Console.ReadLine();
+            var myNum = 8;
+            string statement = "help";
+
+            do
+            {
+                if (guess == statement)
+                {
+                    Console.WriteLine("Lower than 50, but higher than 5.");
+                }
+                else if (Convert.ToInt32(guess) >= 1000)
+                {
+                    Console.WriteLine("Way too high!!");
+                    Console.WriteLine("Consider typing 'help' to get a hint.");
+                }
+                else if (Convert.ToInt32(guess) >= 13)
+                {
+                    Console.WriteLine("Try reducing your number!");
+                }
+                else if (Convert.ToInt32(guess) <= 12 && Convert.ToInt32(guess) >= 9)
+                {
+                    Console.WriteLine("You are really close! A little lower.");
+                }
+                else if (Convert.ToInt32(guess) <= 7 && Convert.ToInt32(guess) >= 4)
+                {
+                    Console.WriteLine("You are really close! A little higher.");
+                }
+                else if (Convert.ToInt32(guess) <= 3)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("Excellent! You guessed my favorite number!!");
+                    break;
+                }
+
+                Console.WriteLine("Enter your next guess:");
+                guess = Console.ReadLine();
+
+               switch (guess)
+                {
+                    case "help" :
+                        break;
+                    case "8":
+                        Console.WriteLine("Excellent! You guessed my favorite number!!");
+                        break;
+                }
 
 
-
-
-
-
-
+            }
+            while (guess == statement || Convert.ToInt32(guess) != myNum);*/
 
         }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+       
+
+    
